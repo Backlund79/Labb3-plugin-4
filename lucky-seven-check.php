ea4bdb51ec12aa8abc24cac566efe43e47d988cd
Emil Backlund <emilbacklund@outlook.com>
2020-11-29 11:24:01 +0100

    Added deactivaton for original plugin

--- lucky-seven-check.php
+++ lucky-seven-check.php
@@ -4,15 +4,24 @@
 * Plugin Name: Lucky Seven 777 validator
 * Author: Emil
 */
-
-
+register_activation_hook(__FILE__, 'check_for_lucky_seven');
+function check_for_lucky_seven()
+{
+    if (is_plugin_active('labb3-plugin-2/777.php')) {
+        add_action('update_option_active_plugins', 'deactivate_lucky_seven');
+    }
+}
+function deactivate_lucky_seven()
+{
+    deactivate_plugins('labb3-plugin-2/777.php');
+}
 
 require 'acf-field-data.php';
-include ABSPATH . '/wp-content/plugins/labb3-plugin-2/777.php';
+// include ABSPATH . '/wp-content/plugins/labb3-plugin-2/777.php';
 
 
 
-class Validate extends Is_seven
+class Validate
 {
     public $test_string;
 
