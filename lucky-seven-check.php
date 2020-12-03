47d51955743617abc2f8a90765bbe4d50960fa90
Emil Backlund <emilbacklund@outlook.com>
2020-12-03 14:40:20 +0100

    test done

--- lucky-seven-check.php
+++ lucky-seven-check.php
@@ -6,31 +6,50 @@
 *
 */
 
-register_activation_hook(__FILE__, 'check_for_lucky_seven');
-function check_for_lucky_seven()
+// register_activation_hook(__FILE__, 'check_for_lucky_seven');
+// function check_for_lucky_seven()
+// {
+//     if (is_plugin_active('labb3-plugin-2/777.php')) {
+//         add_action('update_option_active_plugins', 'deactivate_lucky_seven');
+//     }
+// }
+// function deactivate_lucky_seven()
+// {
+//     deactivate_plugins('labb3-plugin-2/777.php');
+// }
+
+// include ABSPATH . '/wp-content/plugins/labb3-plugin-2/777.php';
+
+function assert_if_seven_works()
 {
-    if (is_plugin_active('labb3-plugin-2/777.php')) {
-        add_action('update_option_active_plugins', 'deactivate_lucky_seven');
+    
+
+    // Parameters
+    $six = '123456';
+    $seven = '1234567';
+    $nine = '123456789';
+
+    
+    
+    function test_func($string, $expected_val)
+    {
+        $test_seven = new Is_seven;
+        $test_seven->set_string_to_check($string);
+        $calc_value = $test_seven->is_seven_characters_long();
+        $converted_res = $expected_val ? 'sant' : 'falskt';
+        $converted_res2 = $calc_value ? 'sant' : 'falskt';
+
+        if ($calc_value === $expected_val) {
+            echo '<p style="color: red; padding-left: 1rem" > Skickat värde = ' . $string . '<br> Förväntat värde = ' . $converted_res . '<br> Beräknat värde = ' . $converted_res2 . '<br> Lyckat test </p><br>';
+        } else {
+            echo '<p style="color: green; padding-left: 1rem" >Skickat värde = ' . $string . '<br> Förväntat värde = ' . $converted_res . '<br> Beräknat värde = ' . $converted_res2 . ' <br> Misslyckat test </p><br>';
+        }
     }
-}
-function deactivate_lucky_seven()
-{
-    deactivate_plugins('labb3-plugin-2/777.php');
-}
-
-include ABSPATH . '/wp-content/plugins/labb3-plugin-2/777.php';
-
-$test1 = '123456';
-$test2 = '1234567';
-
-
-function yes($param1)
-{
-    var_dump($param1);
+    test_func($six, false);
+    test_func($seven, true);
+    test_func($nine, false);
 }
 
-if (isset($_GET['testrun'])) {
-    if (function_exists($_GET['testrun'])) {
-        $_GET['testrun']($_GET['validate']);
-    }
+if (isset($_GET['testrun']) && $_GET['testrun'] === 'yes') {
+    add_action('wp_body_open', 'assert_if_seven_works');
 }
